1a526df9e158b64ee4efda5a98d4b10b393cbd1f
Kirill Kreslavski <kreslavskij_kd@crc.ru>
2025-07-02 17:20:49 +0300

    initial commit

---
+++ src/main/kotlin/ru/kreslavski/family/dinnertime/FamilyDinnerTimeApplication.kt
@@ -0,0 +1,11 @@
+package ru.kreslavski.family.dinnertime
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FamilyDinnerTimeApplication
+
+fun main(args: Array<String>) {
+	runApplication<FamilyDinnerTimeApplication>(*args)
+}
---
+++ src/test/kotlin/ru/kreslavski/family/dinnertime/FamilyDinnerTimeApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.kreslavski.family.dinnertime
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FamilyDinnerTimeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
